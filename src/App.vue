75afa3cfc75f658b32378ff8e2f6828c6ae4d98a
cpy <66666666@qq.com>
2024-04-22 21:36:28 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <a-layout>
+    <a-layout-header class="header">Header</a-layout-header>
+    <a-layout-content class="content">Content</a-layout-content>
+    <a-layout-footer class="footer">Footer</a-layout-footer>
+  </a-layout>
+</template>
+
+<style scoped>
+.header {
+  height: 50px;
+  background-color: skyblue;
+}
+
+.content {
+  height: 500px;
+  background-color: aquamarine;
+}
+.footer{
+  height: 30px;
+  background-color: white;
+}
+
+</style>
